d3f33ebb3311ddb7d97ceb4b7030599944e53b36
三納　楓真 <146901330+SANNO-FUUMA@users.noreply.github.com>
2024-07-04 16:55:41 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+
+class oya
+{
+public:
+	virtual void Voice() {
+		printf("あにまる¥n");
+	};
+};
+
+class kodomoA : public oya
+{
+public:
+	void Voice() {
+		printf("子供Aっす\n");
+	};
+};
+
+class kodomoB : public oya
+{
+public:
+	void Voice() {
+		printf("子供Bっす\n");
+	};
+};
+
+int main(void) {
+
+	oya* koll[2] = { new kodomoA,new kodomoB };
+
+	koll[0]->Voice();
+	koll[1]->Voice();
+
+	delete koll[0];
+	delete koll[1];
+
+	return 0;
+}
+
